1f7817bbdde2ecf74231bf75e75e084157af0167
jzy <jzy@wangyachendeMacBook-Pro.local>
2018-03-10 01:47:50 +0800

    Initial Commit

---
+++ TetrisAi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TetrisAi
+//
+//  Created by jzy on 2018/3/10.
+//  Copyright © 2018年 jzy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
